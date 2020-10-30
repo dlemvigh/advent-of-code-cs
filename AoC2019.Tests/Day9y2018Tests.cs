3c16047d6c420ae233bb605e528d2a684780a87f
David Lemvigh <dle@siteimprove.com>
2020-10-30 11:05:10 +0100

    2019 Day 9 solution

---
+++ AoC2019.Tests/Day9y2018Tests.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Day9y2018;
+using Xunit;
+
+namespace AoC2019.Tests
+{
+    public class Day9y2018Tests
+    {
+				[Fact]
+				public void MarbleManiaTests() {
+					var result = ListNode.MarbleMania(9, 25);
+					Assert.Equal(32, result);
+				}
+
+				[Theory]
+				[InlineData(10, 1618, 8317)]
+				[InlineData(13, 7999, 146373)]
+				[InlineData(17, 1104, 2764)]
+				[InlineData(21, 6111, 54718)]
+				[InlineData(30, 5807, 37305)]
+				public void MarbleMania_InputPlayersAndTurnNumber_ExpectedHighscore(int players, int turns, int expected) {
+					var result = ListNode.MarbleMania(players, turns);
+					Assert.Equal(expected, result);
+				}
+
+				[Fact]
+				public void MarbleMania_Part1() {
+					var result = ListNode.MarbleMania(404, 71852);
+					Assert.Equal(434674, result);
+				}
+
+				[Fact]
+				public void MarbleMania_Part2() {
+					var result = ListNode.MarbleMania(404, 7185200);
+					Assert.Equal(3653994575, result);
+				}
+    }
+}
---
+++ AoC2019/Day9y2018.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Linq;
+
+namespace Day9y2018 {
+	public class ListNode {
+		public int value;
+		public ListNode next;
+		public ListNode prev;
+
+		public ListNode(int value) {
+			this.value = value;
+		}
+
+		public ListNode AddAfter(int value) {
+			var node = new ListNode(value);
+			
+			node.prev = this;
+			node.next = this.next;
+
+			this.next.prev = node;
+			this.next = node;
+
+			return node;
+		}
+
+		public ListNode Remove() {
+			var prev = this.prev;
+			var next = this.next;
+
+			prev.next = next;
+			next.prev = prev;
+
+			return next;
+		}
+
+		public static ListNode Init() {
+			var node = new ListNode(0);
+			node.next = node;
+			node.prev = node;
+			return node;
+		}
+
+
+		public static long MarbleMania(int players, int turns) {
+			// Console.WriteLine("Marbles");
+			var init = ListNode.Init();
+			var current = init;
+			var scores = new long[players];
+
+			Action<int> print = (turn) => {
+				Console.Write($"[{turn}] ");				
+				var it = init;
+				do {
+					Console.Write(it == current ? $"({it.value}) " : $"{it.value} ");
+					it = it.next;
+				} while(it != init);				
+				Console.Write("\n");
+			};
+
+			for(int i = 1, player = 0; i <= turns; i++, player = (player + 1) % players) {
+				if (i % 23 == 0) {
+					scores[player] += i;
+					current = current.prev.prev.prev.prev.prev.prev.prev;
+					scores[player] += current.value;
+					current = current.Remove();
+				} else {
+					current = current.next.AddAfter(i);
+				}
+				// print(i);
+			}
+			return scores.Max();
+		}
+	}
+
+	
+}
